7c1fecda4042ec2aba55956264dabec615fd8fef
ahmedhmedo73 <ahmedhmedo73@gmail.com>
2024-04-02 05:12:57 +0200

    change og meta depends on page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,5 +10,21 @@ import { RouterModule, RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'test-ssr';
+
+  constructor(private meta: Meta, private title: Title) {}
+  ngOnInit(): void {
+    this.title.setTitle('init page');
+    this.meta.updateTag({
+      property: 'og:title',
+      content: 'My Awesome Angular 17 SSR App',
+    });
+    this.meta.updateTag({
+      property: 'og:description',
+      content: 'A detailed description of my Angular 17 SSR app content.',
+    });
+    this.meta.updateTag({
+      property: 'og:image',
+      content: 'https://example.com/my-image.jpg',
+    });
+  }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-products',
   standalone: true,
   imports: [],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrl: './products.component.scss',
 })
 export class ProductsComponent {
-
+  constructor(private meta: Meta, private title: Title) {}
+  ngOnInit(): void {
+    this.title.setTitle('My Awesome Angular 17 SSR App');
+    this.meta.updateTag({
+      property: 'og:title',
+      content: 'My Awesome Angular 17 SSR App',
+    });
+    this.meta.updateTag({
+      property: 'og:description',
+      content: 'A detailed description of my Angular 17 SSR app content.',
+    });
+    this.meta.updateTag({
+      property: 'og:image',
+      content: 'https://example.com/my-image.jpg',
+    });
+  }
 }
