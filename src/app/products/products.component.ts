3950f855d2a6b24ae9534f07732aac1697b2c4aa
ahmedhmedo73 <ahmedhmedo73@gmail.com>
2024-04-02 05:32:21 +0200

    angular 15

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -11,18 +11,8 @@ import { Meta, Title } from '@angular/platform-browser';
 export class ProductsComponent {
   constructor(private meta: Meta, private title: Title) {}
   ngOnInit(): void {
-    this.title.setTitle('My Awesome Angular 17 SSR App');
-    this.meta.updateTag({
-      property: 'og:title',
-      content: 'My Awesome Angular 17 SSR App',
-    });
-    this.meta.updateTag({
-      property: 'og:description',
-      content: 'A detailed description of my Angular 17 SSR app content.',
-    });
-    this.meta.updateTag({
-      property: 'og:image',
-      content: 'https://cdn3.iconfinder.com/data/icons/transparent-on-dark-grey/500/icon-02-512.png',
-    });
+    setTimeout(() => {
+      
+    }, 2000);
   }
 }
